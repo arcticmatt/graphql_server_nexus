a817ba7b7629b74a59b8b5a3753eb0087a0e3ed2
Matt Lim <mlimcollege@gmail.com>
2020-09-26 10:52:48 -0700

    Add preliminary star wars schema and data.

--- src/starWarsSchema.ts
+++ src/starWarsSchema.ts
@@ -1,61 +1,144 @@
 import {
   arg,
   enumType,
-  intArg,
   interfaceType,
   makeSchema,
   objectType,
   queryType,
   stringArg,
 } from "@nexus/schema";
+import { getDroid, getFriends, getHero, getHuman } from "./StarWarsData";
 
-const Node = interfaceType({
-  name: "Node",
+const Episode = enumType({
+  name: "Episode",
+  description: "One of the films in the Star Wars Trilogy",
+  members: [
+    {
+      name: "NEW_HOPE",
+      value: 4,
+      description: "Released in 1977.",
+    },
+    {
+      name: "EMPIRE",
+      value: 5,
+      description: "Released in 1980.",
+    },
+    {
+      name: "JEDI",
+      value: 6,
+      description: "Released in 1983.",
+    },
+  ],
+});
+
+const Character = interfaceType({
+  name: "Character",
+  description: "A character in the Star Wars Trilogy",
   definition(t) {
-    t.id("id", { description: "Unique identifier for the resource" });
-    t.resolveType(() => null);
+    t.string("id", { description: "The id of the character." });
+    t.string("name", {
+      description: "The name of the character.",
+      nullable: true,
+    });
+    t.list.field("friends", {
+      description:
+        "The friends of the character, or an empty list if they have none.",
+      type: Character,
+      list: [false],
+      nullable: true,
+      resolve: (character) => {
+        return getFriends(character);
+      },
+    });
+    t.list.field("appearsIn", {
+      description: "Which movies they appear in.",
+      type: Episode,
+      list: [false],
+      nullable: true,
+    });
+    t.string("secretBackstory", {
+      description: "All secrets about their past.",
+      nullable: true,
+      resolve: () => {
+        throw new Error("secretBackstory is secret");
+      },
+    });
+    t.resolveType((character) => character.type);
   },
 });
 
-const Account = objectType({
-  name: "Account",
+const Human = objectType({
+  name: "Human",
+  description: "A humanoid creature in the Star Wars universe.",
   definition(t) {
-    t.implements(Node); // or t.implements("Node")
-    t.string("username");
-    t.string("email");
+    // Don't think I can change the field descriptions, because modify does not exist.
+    t.implements(Character);
+    t.string("homePlanet", {
+      description: "The home planet of the human, or null if unknown",
+      nullable: true,
+    });
   },
 });
 
-const StatusEnum = enumType({
-  name: "StatusEnum",
-  members: ["ACTIVE", "DISABLED"],
+const Droid = objectType({
+  name: "Droid",
+  description: "A mechanical creature in the Star Wars universe.",
+  definition(t) {
+    // Don't think I can change the field descriptions, because modify does not exist.
+    t.implements(Character);
+    t.string("primaryFunction", {
+      description: "The primary function of the droid.",
+      nullable: true,
+    });
+  },
 });
 
 const Query = queryType({
   definition(t) {
-    t.field("account", {
-      type: Account, // or "Account"
+    t.field("hero", {
+      type: Character,
+      nullable: true,
       args: {
-        name: stringArg(),
-        status: arg({ type: "StatusEnum" }),
+        episode: arg({
+          type: Episode,
+          description:
+            "If omitted, returns the hero of the whole saga. If provided, returns the hero of that particular episode.",
+        }),
+      },
+      resolve(_root, args) {
+        return getHero(args.episode);
       },
     });
-    t.list.field("accountsById", {
-      type: Account, // or "Account"
+    t.field("human", {
+      type: Human,
+      nullable: true,
       args: {
-        ids: intArg({ list: true }),
+        id: stringArg({ description: "id of the human", nullable: false }),
+      },
+      resolve(_root, args) {
+        return getHuman(args.id);
+      },
+    });
+    t.field("droid", {
+      type: Droid,
+      nullable: true,
+
+      args: {
+        id: stringArg({ description: "id of the droid", nullable: false }),
+      },
+      resolve(_root, args) {
+        return getDroid(args.id);
       },
     });
   },
 });
 
-// Recursively traverses the value passed to types looking for
-// any valid Nexus or graphql-js objects to add to the schema,
-// so you can be pretty flexible with how you import types here.
 const schema = makeSchema({
-  types: [Account, Node, Query, StatusEnum],
-  // or types: { Account, Node, Query }
-  // or types: [Account, [Node], { Query }]
+  types: [Episode, Character, Human, Droid, Query],
+  outputs: {
+    schema: `${__dirname}/../generated/schema.graphql`,
+    typegen: `${__dirname}/../generated/types.d.ts`,
+  },
 });
 
 export default schema;
